7d9dfce1106848ec14241b42d7a1c0ca45517d32
YagogesKRYEG <yagoap2001@gmail.com>
2019-05-14 15:28:37 -0300

    Criaçao do pacote persistencia e da classe CRUDCliente e o metodo do seu corpo dia 14/05/2019

---
+++ src/com/prjcadcliente/persistencia/CRUDCliente.java
@@ -0,0 +1,37 @@
+package com.prjcadcliente.persistencia;
+
+import java.util.List;
+
+import com.prjcadcliente.dominio.Cliente;
+
+/**
+ * <b> CRUDCliente</b><br>
+ * Essa clase permite manipular as informações do cliente.Aqui você enecontrara os seguintes comados
+ * @author yago.assilva
+ *<ul>
+ *<li>Cadastro</li>
+ *<li>Pesquisar por nome e por id</li>
+ *<li>Atualizar</li>
+ *<li>declarar</li>
+ */
+public class CRUDCliente {
+	
+	public String cadastrar(Cliente cliente) {
+		return null;
+	}
+	public String atualizar(Cliente cliente) {
+		return null;
+	}
+	public String deletar(Cliente cliente) {
+		return null;
+	}
+	public List<Cliente> pesquisarporNome(String nome) {
+		return null;
+	}
+	public List<Cliente> pesquisarporId(int id) {
+		return null;
+	}
+	public List<Cliente> pesquisarTodos(){
+		return null;
+	}
+}
