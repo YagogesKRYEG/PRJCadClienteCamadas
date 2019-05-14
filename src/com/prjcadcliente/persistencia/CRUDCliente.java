07dbd6d903495eeda0c0a6d15b8acb9271776b93
YagogesKRYEG <yagoap2001@gmail.com>
2019-05-14 17:23:29 -0300

    Criação do corpo do metodo Atualizar do CRUD dia 14/05/2019 end day

--- src/com/prjcadcliente/persistencia/CRUDCliente.java
+++ src/com/prjcadcliente/persistencia/CRUDCliente.java
@@ -55,7 +55,33 @@ public class CRUDCliente {
 		return msg;
 	}
 	public String atualizar(Cliente cliente) {
-		return null;
+		String msg;
+		//Criação dos objetos para a conexão com o banco os dados
+		try {
+		 Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+		 con = DriverManager.getConnection("jdbc:mysql://localhost:3307/clientedb","root","");
+		 String Consulta = "UPDATE tbcliente SET (nome=?,email=?,telefone=?,idade=? WHERE id=?";
+		 pst = con.prepareStatement(Consulta);
+		 pst.setString(1, cliente.getNome());
+		 pst.setString(2, cliente.getEmail());
+		 pst.setString(3, cliente.getTelefone());
+		 pst.setInt(4, cliente.getIdade());
+		 int r = pst.executeUpdate();
+		 if(r > 0)
+			 msg = "Atualizado com sucesso";
+		 else
+			 msg="Não foi possivel Atualizar!!!";
+	}
+		catch(SQLException ex) {
+			msg ="Erro ao tentar Atualizar"+ex.getMessage();
+		}
+		catch(Exception e) {
+			msg = "Erro inesperado:"+e.getMessage();
+		}
+		finally {
+			try{con.close();}catch(Exception e) {e.printStackTrace();}
+		} 
+		return msg;
 	}
 	public String deletar(Cliente cliente) {
 		return null;
